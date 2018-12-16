bcaffbb1eda1bcfbc004fad2800f9cb729f2ed8c
Phillip Scott Givens <psgivens11@yahoo.com>
2018-12-16 12:39:55 -0800

    Gat page looking like an application with full height.

--- webapp/src/app/components/Authenticated.tsx
+++ webapp/src/app/components/Authenticated.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { NavLink } from 'react-router-dom';
 import * as container from './authenticated/authenticatedContainer'
 import MainMenu from './MainMenu';
 
@@ -11,48 +12,21 @@ type ThisProps =
 const Authenticated: React.SFC<ThisProps> = ({ children, logout } : ThisProps ) => {
   return (<>    
       <MainMenu />
-      <div className='FullHeight'>
+      <div id='authenticated'>
         <aside id="sidebar"> 
-        <p className="menu-label">
-    General
-  </p>
-  <ul className="menu-list">
-    <li><a>Dashboard</a></li>
-    <li><a>Customers</a></li>
-  </ul>
-  <p className="menu-label">
-    Administration
-  </p>
-  <ul className="menu-list">
-    <li><a>Team Settings</a></li>
-    <li>
-      <a className="is-active">Manage Your Team</a>
-      <ul>
-        <li><a>Members</a></li>
-        <li><a>Plugins</a></li>
-        <li><a>Add a member</a></li>
-      </ul>
-    </li>
-    <li><a>Invitations</a></li>
-    <li><a>Cloud Storage Environment Settings</a></li>
-    <li><a>Authentication</a></li>
-  </ul>
-  <p className="menu-label">
-    Transactions
-  </p>
-  <ul className="menu-list">
-    <li><a>Payments</a></li>
-    <li><a>Transfers</a></li>
-    <li><a>Balance</a></li>
-  </ul>
-        
-
+          <div>
+          <p className="menu-label">
+            Services
+          </p>
+          <ul className="menu-list">
+            <li><NavLink activeClassName="is-active" to="/Home">Home</NavLink></li>
+            <li><NavLink activeClassName="is-active" to="/Pomodoros">Pomodoros</NavLink></li>
+            <li><NavLink activeClassName="is-active" to="/Ping">Ping</NavLink></li>
+            <li><NavLink activeClassName="is-active" to="/Values">Values</NavLink></li>
+          </ul>
+          </div>       
         </aside>
-        <div id="nextbar" className='FullHeight' >
-          <div  className='FancyBorder FancyBorder-red'>        
-            {children}
-          </div>
-        </div>
+        {children}
       </div>
     </>);
   }
